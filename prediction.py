54fe8dcd588d732817f599c28382b774e465f6f3
Philippe Pepin <aleas@publiinfo.com>
2025-05-02 21:40:14 -0400

    Add logging and cron job management to satellite prediction script; create location file

---
+++ prediction.py
@@ -0,0 +1,135 @@
+#!/home/lnxadmin/.local/share/pipx/venvs/polar2grid/bin/python3
+
+from skyfield.api import load, Topos, EarthSatellite
+from skyfield.iokit import parse_tle_file
+import json
+import datetime
+import numpy as np
+import logging
+from crontab import CronTab
+import argparse
+import os
+
+########## Parameters ######################
+
+parser = argparse.ArgumentParser(
+                    prog='retrieve_jpss_products',
+                    description='This program downloads JPSS data from the cloud',
+                    epilog='Text at the bottom of help')
+
+parser.add_argument('--satellite', type=str, required=True, help='Name of the satellite')
+parser.add_argument('--predict', type=int, required=False, help='How far ahead to predict in hours', default=72)
+parser.add_argument('--clean', action=argparse.BooleanOptionalAction, help='Cleanup old cron jobs before starting')
+args = parser.parse_args()
+
+
+### Configure logging
+
+##### Functions ######################
+
+
+#### Variables
+
+SatelliteToPredict = args.satellite
+clean_CronJobs = args.clean
+HoursToPredict = args.predict
+
+ts = load.timescale()
+num_samples = 1000
+
+SatToPredictID = '0'
+
+if SatelliteToPredict == 'NOAA 21':
+    SatToPredictID = 54234
+    SatShortName = 'n21'
+elif SatelliteToPredict == 'NOAA 20':
+    SatToPredictID = 43013
+    SatShortName = 'n20'
+elif SatelliteToPredict == 'SNPP':
+    SatToPredictID = 37849
+    SatShortName = 'snpp'
+else:
+    raise ValueError(f"Unknown satellite: {SatelliteToPredict}")
+
+
+max_days = 7.0         # download again once 7 days old
+TleFileName = 'weather.tle'  # custom filename, not 'gp.php'
+
+base = 'https://celestrak.org/NORAD/elements/gp.php'
+url = base + '?GROUP=weather&FORMAT=tle'
+
+if not load.exists(TleFileName) or load.days_old(TleFileName) >= max_days:
+    load.download(url, filename=TleFileName)
+
+
+with load.open(TleFileName) as f:
+    satellites = list(parse_tle_file(f, ts))
+
+print('Loaded', len(satellites), 'satellites')
+
+by_number = {sat.model.satnum: sat for sat in satellites}
+
+if SatToPredictID in by_number:
+    satellite = by_number[SatToPredictID]
+else:
+    raise KeyError(f"Satellite ID {SatToPredictID} not found in the TLE data.")
+
+# Load the coordinates from a file named location.txt line 1 is latitude and line 2 is longitude
+# The file should be in the same directory as this script
+try:
+    with open('location.txt', 'r') as file:
+        lines = file.readlines()
+        latitude, longitude = map(float, lines[:2])  # Read the first two lines as latitude and longitude
+        location = Topos(latitude_degrees=latitude, longitude_degrees=longitude)
+except (FileNotFoundError, ValueError, IndexError) as e:
+    raise RuntimeError("Failed to load coordinates from location.txt. Ensure the file exists and contains valid latitude and longitude.") from e
+
+
+now = datetime.datetime.now(datetime.timezone.utc)
+start_time = ts.utc(now.year, now.month, now.day, now.hour, now.minute, now.second)
+end_datetime = now + datetime.timedelta(hours=HoursToPredict)
+end_time = ts.utc(end_datetime.year, end_datetime.month, end_datetime.day,
+                    end_datetime.hour, end_datetime.minute, end_datetime.second)
+
+# Predict the satellite events when its altitude is above 10 degrees.
+# find_events returns: times of events and an array of event codes (0: rise, 1: culmination, 2: set)
+#t_events, events = satellite.find_events(location, t, altitude_degrees=10.0)
+
+t_events, events = satellite.find_events(location, start_time, end_time, altitude_degrees=10.0)
+
+user_home = os.path.expanduser("~")
+
+# Process the events in groups of three: each pass should give you a rising, culmination, and setting.
+
+local_tz = datetime.datetime.now().astimezone().tzinfo
+
+cron = CronTab(user=True)  # Use the current user's crontab
+
+# Remove all existing cron jobs
+if clean_CronJobs is True :
+    logging.info("Removing all existing cron jobs...")
+    cron.remove_all()
+    cron.write()
+
+
+print("Predicted satellite passes:")
+for i in range(0, len(events) // 3 * 3, 3):
+    if events[i] == 0 and events[i+1] == 1 and events[i+2] == 2:
+        t_rise = t_events[i].astimezone(local_tz)
+        t_culm = t_events[i+1].astimezone(local_tz)
+        t_set = t_events[i+2].astimezone(local_tz)
+        Estdelivery_Time = (t_events[i+2] + datetime.timedelta(hours=2)).astimezone(local_tz)
+        # Format the times for the cron job
+        cron_hour = Estdelivery_Time.hour
+        cron_minute = Estdelivery_Time.minute
+        cron_day = Estdelivery_Time.day
+        cron_month = Estdelivery_Time.month
+        cron_year = Estdelivery_Time.year
+        t_rise_str = t_rise.strftime('%Y-%m-%dT%H:%M:%S')
+        # Create a new cron job
+        job = cron.new(command=f'cd' + user_home + '/tmp && ' + user_home + '/scripts/retrieve_jpss_products.py --satellite ' + SatShortName + ' --start '+ t_rise_str + ' --duration 15 --clean')
+        # Set the time for the cron job
+        job.setall(f"{cron_minute} {cron_hour} {cron_day} {cron_month} *")
+        # Write the cron job to the crontab
+        cron.write()
+        print(f"Cron job created to run at {Estdelivery_Time.strftime('%Y-%m-%d %H:%M:%S')} for pass at: {t_rise_str}") 
--- retrieve_jpss_products.py
+++ retrieve_jpss_products.py
@@ -1,5 +1,9 @@
-#version 1.0
+#!/home/lnxadmin/.local/share/pipx/venvs/polar2grid/bin/python3
 
+#version 1.0
+# important: set the timezone
+
+import time
 import boto3
 import os
 import argparse
@@ -8,6 +12,8 @@ from datetime import datetime, timedelta, timezone
 import subprocess
 from botocore import UNSIGNED
 from botocore.client import Config
+from PIL import Image
+import logging
 
 ######### Parameters ######################
 
@@ -22,23 +28,35 @@ parser.add_argument('--duration', type=int, required=True, help='Duration in min
 parser.add_argument('--clean', action=argparse.BooleanOptionalAction, help='Cleanup raw directory before starting')
 args = parser.parse_args()
 
+######## Configure logging #####################
+log_file = "/var/log/retrieve_jpss_products.log"
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logging.info("Starting the JPSS retrieval script")
+logging.info("Arguments: " + str(args))
+
 ########## Variables #############################
+
+# Set the maximum image pixels to None to avoid the DecompressionBombError
+Image.MAX_IMAGE_PIXELS = None
 
 satellite = args.satellite
 clean_raw_data = args.clean
-
 
 #Calculate Start Time
 LocalStartTime = datetime.strptime(args.start, '%Y-%m-%dT%H:%M:%S')
 ZuluStartTime = LocalStartTime.astimezone(timezone.utc)
 StartTime = int(ZuluStartTime.strftime('%H%M%S0'))
-print(f'UTC Start Time: {StartTime}')
+logging.info(f'UTC Start Time: {StartTime}')
 
 #Calculate End Time
 EndTime = ZuluStartTime
 EndTime += timedelta(minutes=args.duration)
 EndTime = int(EndTime.strftime('%H%M%S0'))
-print(f'UTC End Time: {EndTime}')
+logging.info(f'UTC End Time: {EndTime}')
 # Create an S3 client with unsigned configuration
 s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 
@@ -58,15 +76,21 @@ ProductList = ['VIIRS-IMG-GEO-TC',
             'VIIRS-M4-SDR',
             'VIIRS-M5-SDR',
             'VIIRS-M7-SDR',
+            'VIIRS-M10-SDR',
             'VIIRS-M11-SDR',
-            'VIIRS-M13-SDR',
+            'VIIRS-M13-SDR', 
+            'VIIRS-M15-SDR',
             'VIIRS-MOD-GEO-TC',
             'VIIRS-DNB-GEO',
             'VIIRS-DNB-SDR'
             ]
 polar2grid_home = os.environ.get('POLAR2GRID_HOME')
 
-base_path = polar2grid_home + '/raw_data/'
+ProductsToMake = 'true_color adaptive_dnb i03 i05 i04'
+
+base_path = '/srv/raw_data/'
+
+user_home = os.path.expanduser("~")
 
 match satellite:
     case 'n21':
@@ -78,6 +102,7 @@ match satellite:
 
 ############## Functions ##################
 
+# Function to remove all files in a directory
 def remove_all_files(folder_path):
     try:
         # Check if the folder exists
@@ -93,24 +118,59 @@ def remove_all_files(folder_path):
                     elif os.path.isdir(file_path):
                         shutil.rmtree(file_path)
                 except Exception as e:
-                    print(f'Failed to delete {file_path}. Reason: {e}')
-            print("Old data has been removed successfully.")
+                    logging.error(f'Failed to delete {file_path}. Reason: {e}')
         else:
-            print("The folder does not exist.")
+            logging.error("The folder "+ folder_path + "does not exist.")
     except Exception as e:
-        print(f'Error: {e}')
+        logging.error(f'Error: {e}')
 
+#Function to convert TIF files to JPEG
+def convert_tif_to_jpeg(directory):
+    try:
+        for filename in os.listdir(directory):
+            if filename.endswith(".tif"):
+                tif_path = os.path.join(directory, filename)
+                jpeg_path = os.path.join("/srv/aws/", os.path.splitext(filename)[0] + ".jpg")
+                try:
+                    with Image.open(tif_path) as img:
+                        img.convert("RGB").save(jpeg_path, "JPEG")
+                        logging.debug(f"Converted {filename} to {jpeg_path}")
+                except Exception as e:
+                    logging.warning(f"Failed to convert {filename}. Reason: {e}")
+    except Exception as e:
+        logging.error(f"Error processing directory {directory}. Reason: {e}")
+
+#Function to remove zero-byte files
+def remove_zero_byte_files(directory):
+    """
+    Removes all zero-byte (0 KB) files from the specified directory.
+    
+    Args:
+        directory (str): The path to the directory where you want to remove zero-byte files.
+    """
+    try:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.path.getsize(file_path) == 0:
+                    os.remove(file_path)
+                    logging.debug(f"Removed zero-byte file: {file_path}")
+        logging.info("All zero-byte files have been removed successfully.")
+    except Exception as e:
+        logging.error(f"An error occurred while removing zero-byte files: {e}")
 
 ############### Main #####################
 
 if clean_raw_data is True :
-    remove_all_files (base_path)
+        remove_all_files(base_path)
+        remove_all_files (user_home + '/tmp/')
+        logging.info("Old data has been removed successfully.")
 
 
-print("Fetching Satellite " + SatelliteName)
+logging.info("Fetching Satellite " + SatelliteName)
 
 for product in ProductList:
-    print('Fetching product ' + product)
+    logging.info('Fetching product ' + product)
     LookupString = product + '/' + date_year + '/' + date_month + '/' + date_day + '/'
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=LookupString)
     if 'Contents' in response:
@@ -121,15 +181,30 @@ for product in ProductList:
             ObservationEndTime = int(FileNameValues[4].lstrip('e'))
             if ObservationStartTime > StartTime and ObservationEndTime < EndTime :
                 FullPath = base_path + FileName
-                #print(FullPath)
                 if os.path.isfile(FullPath):
-                    print("File exist, skipping")
+                    logging.info("File exist, skipping")
                 else:
-                    print("Downloading " + FileName)
+                    logging.info("Downloading " + FileName)
                     s3.download_file(bucket_name, obj['Key'], FullPath)
     else:
-        print("No objects found for product " + product)
+        logging.warning("No objects found for product " + product)
 
 ################# End ######################
 
-print('please run $POLAR2GRID_HOME/bin/polar2grid.sh -r viirs_sdr -w geotiff -f ~/raw_data/*.h5 --num-workers 12 --progress --list-products')
+logging.info("Processing with polar2grid, creating LCC grid")
+
+polar2grid_arguments = user_home + '/.local/bin/polar2grid -r viirs_sdr -g lcc_fit -w geotiff -f ' + base_path + '*.h5 --num-workers 12 --fill-value 0 -p overview night_overview adaptive_dnb i03'
+
+subprocess.run([polar2grid_arguments], shell=True)
+
+time.sleep(3)
+
+logging.info("Removing empty files")
+
+remove_zero_byte_files(user_home + "/tmp/")
+
+logging.info("Converting TIF to JPEG")
+
+convert_tif_to_jpeg(user_home + "/tmp/")
+
+logging.info('script finished at ' + str(datetime.now()))
